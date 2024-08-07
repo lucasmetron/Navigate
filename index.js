51bad7e87c1991d15f6e7dc3a0f1ce490949a137
Lucas Rosa <lucasmetron@gmail.com>
2024-08-07 14:24:24 -0300

    drawer

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+import './gesture-handler';
 /**
  * @format
  */
-
+import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import Navegacao from './src/navegacao/index';
 import {name as appName} from './app.json';
